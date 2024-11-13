51e60f2bb696d60c654eb34af2b4838910cfa860
Rudy <rudyalvs@gmail.com>
2024-11-13 19:53:41 +0100

    MAJ du 13/11/24 20h

--- CNAM24/js/login.js
+++ CNAM24/js/login.js
@@ -2,59 +2,69 @@ var utilisateurs = [
     {
         id: "Rudy",
         password: "mdp1",
-        connexion:"false"
+        connexion:false
     },
     {
         id: "Alexis",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Hugo",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Alexandre",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Olivier",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Noe",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Lucas",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Yasin",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Thomas",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Marouan",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Pierrick",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Ali",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     },
     {
         id: "Johan",
-        password: "mdp1"
+        password: "mdp1",
+        connexion:false
     }
 ]
-
-
 
 function logon(id, mdp){
     console.log(id, mdp)
@@ -65,12 +75,12 @@ function logon(id, mdp){
     for (let i = 0; i < utilisateurs.length; i++) {
         console.log(utilisateurs[i].id, utilisateurs[i].password)
         if(utilisateurs[i].id == id && utilisateurs[i].password == mdp){
-            location.href="/CNAM/Public/CNAM24/accueil.html";
+            location.href="./pages/accueil.html";
             utilisateurs[i].connexion = true
+
             return true
         }else{
             $('#message').text("Erreur, identifiant ou mot de passe n'est pas bon.")
         }
     }
 }
-
